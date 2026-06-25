a5e0948dad6b3bb1d26682970fb5b2455e4e813b
Laukik Waikar <157301589+LAU29004@users.noreply.github.com>
2026-06-26 01:34:30 +0530

    Website fix

--- Website/src/app/login/page.tsx
+++ Website/src/app/login/page.tsx
@@ -20,7 +20,7 @@ export default function LoginPage() {
     console.log("GOOGLE RESPONSE:", credentialResponse);
     try {
       const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/google`,
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/google/`,
         {
           id_token: credentialResponse.credential,
         },
